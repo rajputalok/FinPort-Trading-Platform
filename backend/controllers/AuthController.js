ee3d460241b39a77bd82bff460ec472558435ca5
alok kumar singh <alok83983@gmail.com>
2026-01-16 22:04:58 +0530

    updated user login

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -42,7 +42,9 @@ module.exports.Login = async (req, res, next) => {
      const token = createSecretToken(user._id);
      res.cookie("token", token, {
        withCredentials: true,
-       httpOnly: false,
+       httpOnly: true,
+       secure: true,
+       sameSite: "none",
      });
      res.status(201).json({ message: "User logged in successfully", success: true });
      next()
